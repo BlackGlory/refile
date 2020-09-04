50a9285ed04771c105070e11e22a85fd9bd30d06
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-09-04 10:10:18 +0800

    refactor: move fastify to server.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import fastify from 'fastify'
-import { routes as upload } from '@service/upload'
-
-const IS_PRODUCTION: boolean = true || process.env.NODE_ENV === 'production'
-const PORT: number = Number(process.env.PORT) || 8080
-const HOST: string = process.env.HOST || (IS_PRODUCTION ? '0.0.0.0' : '127.0.0.1')
-
-const server = fastify({ logger: true })
-server.register(upload)
-server.listen(PORT, HOST, (err, address) => {
-  if (err) throw err
-  console.log(`Server listening at ${address}`)
-})
+import './server'
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+import { routes as upload } from '@service/upload'
+
+const IS_PRODUCTION: boolean = true || process.env.NODE_ENV === 'production'
+const PORT: number = Number(process.env.PORT) || 8080
+const HOST: string = process.env.HOST || (IS_PRODUCTION ? '0.0.0.0' : '127.0.0.1')
+
+const server = fastify({ logger: false })
+server.register(upload)
+server.listen(PORT, HOST, (err, address) => {
+  if (err) throw err
+  console.log(`Server listening at ${address}`)
+})
