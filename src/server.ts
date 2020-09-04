2a15bb29a7e19b8be7dd270f0d053a012f1dd73c
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-09-04 19:03:36 +0800

    feat: add pino-pretty for logging

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import fastify from 'fastify'
 import { routes as upload } from '@service/upload'
 import { HOST, PORT } from '@src/config'
 
-const server = fastify({ logger: false })
+const server = fastify({ logger: true })
 server.register(upload)
 server.listen(PORT, HOST, (err, address) => {
   if (err) throw err
