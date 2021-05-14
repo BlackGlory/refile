952c668c8cee7b430d4e6474a05920bd06fa12df
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-05-14 08:15:28 +0000

    chore: upgrade dependencies

--- src/dao/data-in-fs/storage/save-file.ts
+++ src/dao/data-in-fs/storage/save-file.ts
@@ -3,14 +3,14 @@ import { createWriteStream } from 'fs'
 import { createLocation } from './utils/create-location'
 import { createFilename } from './utils/create-filename'
 import { getFileHash } from './utils/get-file-hash'
-import { createTempFilename, ensureDir } from 'extra-filesystem'
+import { createTempName, ensureDir } from 'extra-filesystem'
 import { pipeline } from 'stream'
 import { remove } from 'extra-filesystem'
 import * as path from 'path'
 
 export function saveFile(stream: NodeJS.ReadableStream): Promise<string> {
   return new Promise(async (resolve, reject) => {
-    const tempFilename = await createTempFilename()
+    const tempFilename = await createTempName()
 
     pipeline(
       stream
