7c4730f7172955f310522a7686a2bf3d95b3bb32
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-06-21 18:33:25 +0000

    feat: add /health

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { Registry } from 'prom-client'
 import { routes as admin } from '@services/admin'
 import { routes as refile } from '@services/refile'
 import { routes as robots } from '@services/robots'
+import { routes as health } from '@services/health'
 import { HTTP2, PAYLOAD_LIMIT, NODE_ENV, NodeEnv } from '@env'
 import { Core } from '@core'
 
@@ -27,6 +28,7 @@ export function buildServer() {
   server.register(admin, { Core })
   server.register(refile, { Core })
   server.register(robots)
+  server.register(health)
 
   return server
 }
