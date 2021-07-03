dac3f04f46153ddfec57237938fb9e7b264bf6f9
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-07-03 20:01:49 +0000

    chore: upgrade dependencies

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,6 +1,6 @@
 import { ValueGetter } from 'value-getter'
 import { isNumber } from '@blackglory/types'
-import { Getter } from 'hotypes'
+import { Getter } from 'justypes'
 import { assert } from '@blackglory/errors'
 import { getCache } from '@env/cache'
 import { path as appRoot } from 'app-root-path'
@@ -124,7 +124,7 @@ export const SET_PAYLOAD_LIMIT: Getter<number> =
     .get()
 
 function env(name: string): ValueGetter<string | undefined> {
-  return new ValueGetter(() => process.env[name])
+  return new ValueGetter(name, () => process.env[name])
 }
 
 function toBool(val: string | boolean | undefined): boolean | undefined {
