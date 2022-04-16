6bf3c4237cb42bb3bcba36dd6548a6f20fce4a0e
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-04-16 02:56:23 +0000

    chore: create schemas

--- migrations/data-in-sqlite3/003-optimize.sql
+++ migrations/data-in-sqlite3/003-optimize.sql
@@ -3,7 +3,6 @@
 --------------------------------------------------------------------------------
 
 -- 优化 countReferences
-
 CREATE INDEX idx_refile_reference_file_hash
     ON refile_reference(file_hash);
 
