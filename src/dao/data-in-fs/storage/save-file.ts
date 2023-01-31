8cd8eb17846104409c333ec980c26b24e6b0f7ae
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-01-31 07:05:43 +0000

    refactor: improve readability

--- src/dao/data-in-fs/storage/save-file.ts
+++ src/dao/data-in-fs/storage/save-file.ts
@@ -3,7 +3,7 @@ import { createLocation } from './utils/create-location.js'
 import { createFilename } from './utils/create-filename.js'
 import { getFileHash } from './utils/get-file-hash.js'
 import { createTempName, ensureDir } from 'extra-filesystem'
-import { pipeline } from 'stream'
+import { pipeline } from 'stream/promises'
 import { remove, move } from 'extra-filesystem'
 import * as path from 'path'
 
@@ -11,22 +11,21 @@ export function saveFile(stream: NodeJS.ReadableStream): Promise<string> {
   return new Promise(async (resolve, reject) => {
     const tempFilename = await createTempName()
 
-    pipeline(
-      stream
-    , createWriteStream(tempFilename)
-    , async err => {
-        if (err) {
-          await remove(tempFilename)
-          return reject(err)
-        }
+    try {
+      await pipeline(
+        stream
+      , createWriteStream(tempFilename)
+      )
+    } catch (err) {
+      await remove(tempFilename)
+      return reject(err)
+    }
 
-        const hash = await getFileHash(tempFilename)
-        const location = createLocation(hash)
-        const filename = createFilename(location)
-        await ensureDir(path.dirname(filename))
-        await move(tempFilename, filename)
-        resolve(location)
-      }
-    )
+    const hash = await getFileHash(tempFilename)
+    const location = createLocation(hash)
+    const filename = createFilename(location)
+    await ensureDir(path.dirname(filename))
+    await move(tempFilename, filename)
+    resolve(location)
   })
 }
