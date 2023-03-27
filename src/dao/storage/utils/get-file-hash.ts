33866a5cea2bcc6f047202838a2e65c2afb62ef0
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-03-27 03:21:12 +0000

    refactor: improve readability

--- src/dao/storage/utils/get-file-hash.ts
+++ src/dao/storage/utils/get-file-hash.ts
@@ -1,12 +1,11 @@
 import * as fs from 'fs'
 import * as crypto from 'crypto'
 
-export function getFileHash(filename: string): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const hash = crypto.createHash('sha256')
-    const stream = fs.createReadStream(filename)
-    stream.on('error', err => reject(err))
-    stream.on('data', chunk => hash.update(chunk))
-    stream.on('end', () => resolve(hash.digest('hex')))
-  })
+export async function getFileHash(filename: string): Promise<string> {
+  const hash = crypto.createHash('sha256')
+  const stream = fs.createReadStream(filename)
+  for await (const chunk of stream) {
+    hash.update(chunk as crypto.BinaryLike)
+  }
+  return hash.digest('hex')
 }
